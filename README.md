9ea34e6ed7bb2448e4792ce09b25815e28a33d13
Orinise <larroche.maximilien@gmail.com>
2024-03-23 19:17:22 +0100

    update 0.02

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Site web de benjamin
 
 Update 0.01 Update du fichier JS
+
+######################################
+
+Update 0.02
+
+    modification JS, mosaique et du dossier image
+    JS besoin d'être fini
--- js/script.js
+++ js/script.js
@@ -1,33 +1,31 @@
         //#########################################
-        // Fonction blur
+        // Fonction mosaique
         //######################################### 
-
-        var Element = document.querySelectorAll('.imgal');
         
-        Element.forEach(function(element){
-            element.addEventListener("mouseover",function(event){
-                bluradd(event.target);
-            });
+        // import * as fs from 'fs';
+        var image = document.querySelectorAll('.imgal');
 
-            element.addEventListener("mouseout",function(){
-                blurremove();
-            });
+        image.forEach(function(element) {
+          element.addEventListener("mouseover", function(){
+            alert("dossier");
+
+            const dossier = "../image/Knit";
+            var nb_element = 0;
+
+            fs.readdir(dossier, (err, fichiers) => {
+                if (err) {
+                  alert('Erreur lors de la lecture du dossier :' + err);
+                  return;
+                }
+                alert('Nombre d\'éléments dans le dossier :' + fichiers.length);
+                nb_element = fichiers.length;
+              });
+          });
+          
         });
 
-        function bluradd(element){
-            var allElement = document.querySelectorAll('body > *');
-            allElement.forEach(function (elements){
-                if(elements !== element){
-                    elements.style.filter = 'blur(5px)';
-        }});
-            element.id.style.filter = 'none';
-        }
 
-        function blurremove(){
-            var allElement = document.querySelectorAll('body > *');
-            allElement.forEach(function (element){
-                element.style.filter = 'none';
-            });
-        }
+        
 
+        
     
